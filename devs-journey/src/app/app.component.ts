9cfc598444d2cfd665e9b89f22eb2bd744474e10
tk4500 <tarcisiotrier@gmail.com>
2025-06-05 10:26:17 -0300

    compiled page

--- devs-journey/src/app/app.component.ts
+++ devs-journey/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component , inject, OnInit} from '@angular/core';
 import { ActivatedRoute, RouterOutlet } from '@angular/router';
-import { Button } from 'primeng/button';
-import { GamescreenComponent } from "./gamescreen/gamescreen.component";
 import { Toolbar } from 'primeng/toolbar';
 import { PrimeNG } from 'primeng/config';
 import { BlocklyService } from './blockly/blockly.service';
@@ -9,7 +7,7 @@ import { ExplainComponent } from './explain/explain.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, GamescreenComponent, Toolbar, Button, ExplainComponent],
+  imports: [RouterOutlet, Toolbar, ExplainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
